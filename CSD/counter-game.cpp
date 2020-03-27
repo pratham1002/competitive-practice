1f1cfda5f062d31e85e16b2ecc21ae868e13c98e
Pratham Gupta <f20190051@pilani.bits-pilani.ac.in>
2020-03-27 17:26:30 +0530

    CSD contest

---
+++ CSD/counter-game.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace std::chrono; 
+
+// Complete the counterGame function below.
+vector<long> powers; int size1=0;
+#include <chrono> 
+
+
+void findPowers()
+{
+    long i = 2;
+    for (int j = 1; j <= 63; j++)
+    {
+        powers.push_back(i);
+        i = i * 2;
+    }
+    size1=powers.size();
+}
+
+string counterGame(long n) 
+{   
+    int res = 1;
+
+    while(n > 1)
+    {
+        int flag;
+        int max=size1-2;
+        
+        for (int i = max; i > 0; i--)
+        {
+            if(n == powers[i])
+            {
+                flag = 1;
+                break;
+            }
+            if(powers[i]<n)
+            {   
+                flag = powers[i];
+                max=i-1;
+                break;
+            }
+        }
+
+        if(flag==1)
+            n /= 2;
+        else
+            n -= flag;
+        res++;
+    }
+
+    if (res % 2 == 1)
+        return "Richard";
+    else
+        return "Louise";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+    
+    findPowers();
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        auto start = high_resolution_clock::now();
+        string result = counterGame(n);
+        auto stop = high_resolution_clock::now();
+    
+        auto duration = duration_cast<microseconds>(stop - start); 
+  
+        cout << "Time taken by function: "
+            << duration.count() << " microseconds" << endl; 
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
