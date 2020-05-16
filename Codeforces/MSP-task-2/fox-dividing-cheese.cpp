720792db59a05ec7891824cb914a396507c09209
pratham1002 <f20190051@pilani.bits-pilani.ac.in>
2020-05-17 00:42:39 +0530

    Works till case 7

--- Codeforces/MSP-task-2/fox-dividing-cheese.cpp
+++ Codeforces/MSP-task-2/fox-dividing-cheese.cpp
@@ -9,13 +9,97 @@ int main()
 
     int steps = 0;
 
-    while(a!=b)
+    while(1)
     {
-        if(a == 0 || b == 0)
-        {
-            steps=-1;
+        if (a == b) {
             break;
         }
-        else
+        // if both divisible by same number then dont divide by that
+
+        // first find the larger number
+        if (a > b) {
+            if (a % 2 == 0 && b % 2 != 0) {
+                a /= 2;
+                steps += 1;
+                continue;
+            }
+            if (a % 3 == 0 && b % 3 != 0) {
+                a /= 3;
+                steps += 1;
+                continue;
+            }
+            if (a % 5 == 0 && b % 5 != 0) {
+                a /= 5;
+                steps += 1;
+                continue;
+            }
+            // if number is indivisible;
+            if (a % 2 == 0) {
+                a /= 5;
+                steps += 1;
+                continue;
+            }
+            if (a % 3 == 0) {
+                a /= 3;
+                steps += 1;
+                continue;
+            }
+            if (a % 5 == 0) {
+                a /= 2;
+                steps += 1;
+                continue;
+            }
+
+            break;
+        }
+        else {
+            if (b % 2 == 0 && a % 2 != 0) {
+                b /= 2;
+                steps += 1;
+                continue;
+            }
+            if (b % 3 == 0 && a % 3 != 0) {
+                b /= 3;
+                steps += 1;
+                continue;
+            }
+            if (b % 5 == 0 && a % 5 != 0) {
+                b /= 5;
+                steps += 1;
+                continue;
+            }
+            if (b % 2 == 0) {
+                b /= 2;
+                steps += 1;
+                continue;
+            }
+            if (b % 3 == 0) {
+                b /= 3;
+                steps += 1;
+                continue;
+            }
+            if (b % 5 == 0) {
+                b /= 5;
+                steps += 1;
+                continue;
+            }
+
+            // if number is indivisible;
+            break;
+        }
+
+        // if either a or b is zero then stop
+        if (a == 0 || b == 0) {
+            break;
+        }
     }
+
+    if (a == b) {
+        cout << steps << endl;
+    }
+    else {
+        cout << -1 << endl;
+    }
+
+    return 0;
 }
