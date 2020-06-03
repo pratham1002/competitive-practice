0709e2337f800895fdafc48e7169e20e7a9dbf34
pratham1002 <f20190051@pilani.bits-pilani.ac.in>
2020-06-03 12:50:52 +0530

    final accepteed solution

--- Hackerrank/querying-document.c
+++ Hackerrank/querying-document.c
@@ -5,15 +5,18 @@
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
 char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
-    return *(*(*(document + n - 1) + m - 1) + k - 1);
+    // return *(*(*(document + n - 1) + m - 1) + k - 1);
+    return document[n-1][m-1][k-1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) { 
-    return *(*(document+m - 1) + k - 1);
+    // return *(*(document+m - 1) + k - 1);
+    return document[m-1][k-1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-    return *(document+k-1);
+    // return *(document+k-1);
+    return document[k-1];
 }
 
 char**** get_document(char* text) {
@@ -23,62 +26,88 @@ char**** get_document(char* text) {
     document = (char ****)malloc(sizeof(char ***));     // entire document
     *document = (char ***)malloc(sizeof(char **));      // for paragraphs
     **document = (char **)malloc(sizeof(char *));       // for sentences
-    ***document = (char *)malloc(sizeof(char));         // for word
+    // ***document = (char *)malloc(sizeof(char));         // for word
+    ***document = NULL;
 
     int num_paragraphs = 1;
     int num_sentences = 1;
     int num_words = 1;
-    int current_word_length = 1;
+    int current_word_length = 0;
 
     // enter the first character in the document
-    ****document = *text;
-
-    text++;
-
+    // ****document = *(text);
+    // text += 1;
+    char word[100000];
+    int p = 0;
     while(*(text) != '\0') {
-        char c = *text;
-        text++;
-
+        // char c = *text;
+        // text++;
         // continue the same word 
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-            current_word_length += 1;
-            *(*(*(document + num_paragraphs - 1) + num_sentences - 1) + num_words - 1) = (char *)realloc(*(*(*(document + num_paragraphs - 1) + num_sentences - 1) + num_words - 1), sizeof(char) * current_word_length);
-            *(*(*(*(document + num_paragraphs - 1) + num_sentences - 1) + num_words - 1) + current_word_length - 1) = c;
+        if ((*text >= 'a' && *text <= 'z') || (*text >= 'A' && *text <= 'Z')) {
+            // printf("%c ", *text);
+            // printf("in = %s\n", document[num_paragraphs-1][num_sentences-1][num_words-1]);
+            // current_word_length += 1;
+            // document[num_paragraphs-1][num_sentences-1][num_words-1] = (char *)realloc(document[num_paragraphs-1][num_sentences-1][num_words-1], current_word_length);
+            // document[num_paragraphs-1][num_sentences-1][num_words-1][current_word_length-1] = *text;
+            // printf("current word lentgth = %d, out = %s\n", current_word_length,document[num_paragraphs-1][num_sentences-1][num_words-1]);
+            // text++;
+            word[p++] = *text;
+            text++;
         }
 
         // new word 
-        if (c == ' ') {
-            current_word_length = 0;
+        if (*text == ' ') {
+            // current_word_length = 0;
+            document[num_paragraphs-1][num_sentences-1][num_words-1] = (char *) malloc(sizeof(word));
+            strcpy(document[num_paragraphs-1][num_sentences-1][num_words-1], word);
+            memset(word,0,sizeof(word));
+            p = 0;
             num_words += 1;
 
-            *(*(document + num_paragraphs - 1) + num_sentences - 1) = (char **)realloc(*(*(document + num_paragraphs - 1) + num_sentences - 1), sizeof(char *) * num_words);
+            document[num_paragraphs-1][num_sentences-1] = (char **)realloc(document[num_paragraphs-1][num_sentences-1], sizeof(char *) * num_words);
+            document[num_paragraphs-1][num_sentences-1][num_words-1] = NULL; 
+            text++;
         }
 
         // new sentence
-        if (c == '.') {
+        if (*text == '.') {
+            document[num_paragraphs-1][num_sentences-1][num_words-1] = (char *) malloc(sizeof(word));
+            strcpy(document[num_paragraphs-1][num_sentences-1][num_words-1], word);
+            memset(word,0,sizeof(word));
+            p = 0;
             current_word_length = 0;
             num_words = 1;
             num_sentences += 1;
 
-            *(document + num_paragraphs - 1) = (char ***)realloc(*(document + num_paragraphs - 1), sizeof(char **) * num_sentences);
-            *(*(document + num_paragraphs - 1) + num_sentences - 1) = (char **)realloc(*(*(document + num_paragraphs - 1) + num_sentences - 1), sizeof(char *) * num_words);
+            document[num_paragraphs-1] = (char ***)realloc(document[num_paragraphs-1], sizeof(char **) * num_sentences);
+
+            document[num_paragraphs-1][num_sentences-1] = (char **)malloc(sizeof(char *));
+
+            document[num_paragraphs-1][num_sentences-1][0] = NULL;
+
+            text++;
         }
 
-        if (c == '\n') {
+        if (*text == '\n') {
             current_word_length = 0;
             num_words = 1;
             num_sentences = 1;
             num_paragraphs += 1;
 
             document = (char ****)realloc(document, sizeof(char ***) * num_paragraphs);
-            *(document + num_paragraphs - 1) = (char ***)realloc(*(document + num_paragraphs - 1), sizeof(char **) * num_sentences);
-            *(*(document + num_paragraphs - 1) + num_sentences - 1) = (char **)realloc(*(*(document + num_paragraphs - 1) + num_sentences - 1), sizeof(char *) * num_words);
+
+            *(document + num_paragraphs - 1) = (char ***)malloc(sizeof(char **));
+
+            **(document + num_paragraphs - 1) = (char **)malloc(sizeof(char *));
+
+            ***(document + num_paragraphs - 1) = NULL; 
+
+            text++; 
         }
     }
-
+    // printf("\n");
     return document;
 }
-
 
 char* get_input_text() {	
     int paragraph_count;
@@ -125,7 +154,7 @@ void print_paragraph(char*** paragraph) {
 int main() 
 {
     char* text = get_input_text();
-    char ****document = get_document(text);
+    char**** document = get_document(text);
 
     int q;
     scanf("%d", &q);
