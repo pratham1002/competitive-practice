1e521c74c7f682dad913531b6a7751de783e8d06
pratham1002 <f20190051@pilani.bits-pilani.ac.in>
2020-06-05 20:35:04 +0530

    cpp classes easy questions

---
+++ Hackerrank/cpp-language-proficiency/Classes/Easy/classes.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+/*
+Enter code for class Student here.
+Read statement for specification.
+*/
+class Student {
+    public:
+        int age;
+        string first_name;
+        string last_name;
+        int standard;
+
+        int get_age() {
+            return this->age;
+        }
+        string get_first_name() {
+            return this->first_name;
+        }
+        string get_last_name() {
+            return this->last_name;
+        }
+        int get_standard() {
+            return this->standard;
+        }
+
+        void set_age(int age) {
+            this->age = age;
+        }
+        void set_first_name(string first_name) {
+            this->first_name = first_name;
+        }
+        void set_last_name(string last_name) {
+            this->last_name = last_name;
+        }
+        void set_standard(int standard) {
+            this->standard = standard;
+        }
+
+        string to_string() {
+            string res;
+            res.append(std::to_string(this->age));
+            res.push_back(',');
+            res.append(this->first_name);
+            res.push_back(',');
+            res.append(this->last_name);
+            res.push_back(',');
+            res.append(std::to_string(this->standard));
+
+            return res;
+        }
+};
+
+int main() {
+    int age, standard;
+    string first_name, last_name;
+    
+    cin >> age >> first_name >> last_name >> standard;
+    
+    Student st;
+    st.set_age(age);
+    st.set_standard(standard);
+    st.set_first_name(first_name);
+    st.set_last_name(last_name);
+    
+    cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+}
